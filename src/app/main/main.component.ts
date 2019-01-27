50081f5ac57cc3d144986bcced6008e1c0258fa3
Dimitris Mitsiou <dimitrismitsiou@outlook.com>
2019-01-27 15:12:47 +0200

    new version with issues

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subject, Observable, fromEvent } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -14,57 +21,111 @@ export class MainComponent implements OnInit {
   totalPages = 0;
   searchValue = '';
   categories = [];
-  searchResults = [];
-  selectedCategory = '';
+  news = [];
+  selectedCategory = 'all';
 
-  constructor(private http: HttpClient) {
+  getNews = new BehaviorSubject([]);
+  searchChanged: Subject<string> = new Subject<string>();
+  categoryChanged: Subject<string> = new Subject<string>();
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {
     this.http.get('https://newsapi.org/v1/sources').subscribe((res: any) => {
+      this.getNews.next(res.sources);
       this.allSources = res.sources;
-      this.totalPages = Math.ceil(this.allSources.length / 6);
-      this.sourcesForShow = this.allSources.slice(0, 6);
       const tempCategories = res.sources.map(s => s.category);
       this.categories = tempCategories.filter( this.onlyUnique );
+      this.applyQueryParams(this.route.snapshot.queryParams);
     }, error => {
       console.log(error);
     });
-   }
+
+  }
 
   ngOnInit() {
+    this.getNews.subscribe((news: any) => {
+      this.totalPages = Math.ceil(news.length / 6);
+      this.news = news.slice(0, 6);
+      this.page = 0;
+    });
+
+    this.searchChanged.pipe(
+      debounceTime(1000),
+    ).subscribe(term => {
+      if (term.length > 0) {
+        this.getNews.next(this.getNews.getValue().filter(s => s.name.toLowerCase().startsWith(term.toLowerCase())));
+      } else {
+        // allagi logikis gia na krataei to proigoumeno state
+        this.getNews.next(this.getNews.getValue());
+      }
+      this.navigateToRoute({term: term});
+    });
+
+    this.categoryChanged.pipe(
+        debounceTime(500),
+      ).subscribe(cat => {
+        if (cat !== 'all') {
+          this.getNews.next(this.allSources.filter(s => s.category.toLowerCase().startsWith(cat.toLowerCase())));
+        } else {
+          this.getNews.next(this.allSources);
+        }
+        this.navigateToRoute({category: cat});
+      });
   }
 
   onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
   }
 
+  applyQueryParams(queryParams) {
+    if ('category' in queryParams) {
+      this.categoryChanged.next(queryParams.category);
+      this.selectedCategory = queryParams.category;
+    }
+
+    if ('term' in queryParams) {
+      this.searchChanged.next(queryParams.term);
+    }
+
+    if ('page' in queryParams) {
+      this.goToPage(queryParams.page);
+    }
+  }
+
+  navigateToRoute(params) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: params,
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  goToPage(page) {
+    const totalPages = Math.ceil(this.getNews.getValue().length / 6);
+    if (totalPages >= +page) {
+
+    }
+  }
+
   nextPage(page) {
-    this.sourcesForShow = [];
-    this.sourcesForShow = this.allSources.slice((6 * page), (6 * page) + 6);
+    this.news = this.getNews.getValue().slice((6 * page), (6 * page) + 6);
     this.page += 1;
+    this.navigateToRoute({page: this.page + 1});
   }
 
   previousPage(page) {
-    this.sourcesForShow = [];
-    this.sourcesForShow = this.allSources.slice((6 * page), (6 * page) + 6);
+    this.news = this.getNews.getValue().slice((6 * page), (6 * page) + 6);
     this.page -= 1;
+    this.navigateToRoute({page: this.page + 1});
   }
 
   searchSources(event: any) {
     const term = event.target.value;
-    if (term !== '') {
-      this.searchResults = this.allSources.filter(s => s.name.toLowerCase().startsWith(term.toLowerCase()));
-    } else {
-      this.searchResults = [];
-    }
+    this.searchChanged.next(term);
   }
 
   selectCategorySource(event: any) {
     const cat = event.target.value;
-    console.log(cat);
-    if (cat !== 'all') {
-      this.searchResults = this.allSources.filter(s => s.category.toLowerCase().startsWith(cat.toLowerCase()));
-    } else {
-      this.searchResults = [];
-    }
+    this.categoryChanged.next(cat);
   }
 
 }
